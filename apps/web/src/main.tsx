c357af23d9ace23e60b4a8a7179c16e5584ff5c5
Adrian Darian <adrian.the.hactus@gmail.com>
2025-06-16 12:25:12 -0700

    Refactor doctor page with table

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -2,9 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import DoctorPage from './components/doctor'
+
+const RootComponent = window.location.pathname.startsWith('/doctor')
+  ? DoctorPage
+  : App
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RootComponent />
   </StrictMode>,
 )
